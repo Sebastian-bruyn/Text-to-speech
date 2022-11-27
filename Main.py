344a44598ed806fb1410e88e0f512d81c6d969f5
Sebastian-bruyn <sebastian.bruyn@gmail.com>
2022-11-27 23:19:14 +0200

    update

--- Main.py
+++ Main.py
@@ -3,9 +3,15 @@ import os
 from playsound import playsound
 
 
-
+#Text Input
 Text = input()
+
+#Conversion
 TextSound= gTTS(Text)
+
+#File management
 os.remove('text.mp3')
 TextSound.save('text.mp3')
+
+#text output
 playsound('text.mp3')
